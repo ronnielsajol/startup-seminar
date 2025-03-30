897782bcbb891457cf5ed581401246ec5e3dc5ac
Ronniel Daven Sajol <sajolronniel28@gmail.com>
2025-03-30 20:53:29 +0800

    changed database/index.js

--- database/index.ts
+++ database/index.ts
@@ -3,5 +3,5 @@ import postgres from "postgres";
 // import * as schema from "./schema";
 import { DATABASE_URL } from "../config/env";
 
-const client = postgres(DATABASE_URL ?? "");
+const client = postgres(DATABASE_URL ?? "", { prepare: false });
 export const db = drizzle({ client });
