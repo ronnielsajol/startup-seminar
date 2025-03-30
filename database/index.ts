04a3a12d09cf4f8de996a5a782b3ee22a23d54fc
Ronniel Daven Sajol <sajolronniel28@gmail.com>
2025-03-30 20:48:10 +0800

    changed database/index.js

--- database/index.ts
+++ database/index.ts
@@ -1,13 +1,7 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-import * as schema from "./schema";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+// import * as schema from "./schema";
 import { DATABASE_URL } from "../config/env";
 
-const pool = new Pool({
-	connectionString: DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: false, // ✅ Allow self-signed cert
-	},
-});
-
-export const db = drizzle(pool, { schema });
+const client = postgres(DATABASE_URL ?? "");
+export const db = drizzle({ client });
