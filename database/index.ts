6835094b91f0a5dd7778a3b49ed0cd5a4cec638b
Ronniel Daven Sajol <sajolronniel28@gmail.com>
2025-03-30 19:52:04 +0800

    added ssl reject unauthoized

--- database/index.ts
+++ database/index.ts
@@ -5,6 +5,9 @@ import { DATABASE_URL } from "../config/env";
 
 const pool = new Pool({
 	connectionString: DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false, // ✅ Allow self-signed cert
+	},
 });
 
 export const db = drizzle(pool, { schema });
