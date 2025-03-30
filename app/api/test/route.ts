3493312a0512a3c8f594e7ad67cbb5940f8a4a6b
Ronniel Daven Sajol <sajolronniel28@gmail.com>
2025-03-30 21:18:00 +0800

    fixed build error

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,8 +1,5 @@
-import { db } from "@/database";
-import { users } from "@/database/schema";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-	const allUsers = await db.select().from(users);
-	return NextResponse.json({ users: allUsers });
+	return NextResponse.json({ message: "hello" });
 }
